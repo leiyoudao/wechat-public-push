d4427d4d4105e83ff6af7aeae967f1ec39ed56bb
leiyoudao <117031418+leiyoudao@users.noreply.github.com>
2022-10-31 01:08:04 +0800

    Update and rename config/index.cjs to 配置/index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -13,13 +13,13 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '滑县',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '傻吊',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU17252TkWenO2sA7CDoyShXNrQ5UMoyhtzzlEM4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,21 +29,21 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '傻吊', year: '1996', date: '09-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '傻逼', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '515', year: '2020', date: '09-03',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
+      // 已经傻了xxxx天了的配置
       customizedDateList: [
-        // 在一起的日子
+        // 你已经活了
         { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
+        // 你出生了
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
